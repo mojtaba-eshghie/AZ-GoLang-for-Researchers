72888a4c375d02ae6a1405496808c9b50da3185c
Mojtaba Eshghie <mojtaba.eshghi@ut.ac.ir>
2020-08-03 16:34:15 +0430

    adding new session on implementing our own version of Writer interface

--- 12/main.go
+++ 12/main.go
@@ -7,7 +7,21 @@ import (
 	"fmt"
 )
 
+type logWriter struct {
 
+}
+
+func (logWriter) Write(bs []byte) (int, error){
+	//just by defining the logWriter and associating logWriter to this Write
+	// function, logWriter is now implementing the Writer interface.
+	fmt.Println(string(bs))
+
+
+	// to implement the return we have to take a look at the Write interface documentation 
+	// to implement it according to the docs
+	// here, we should return (number of bytes written, error)
+	return len(bs), nil
+}
 
 func main() {
 	resp, err := http.Get("http://google.com/")
@@ -19,4 +33,13 @@ func main() {
 
 	io.Copy(os.Stdout, resp.Body)
 
+	////////////////////////////// second part //////////////////////////
+
+	//let's instantiate this struct now
+	lw := logWriter{}
+	
+	//let's do this:
+	io.Copy(lw, resp.Body)
+
 }
+
